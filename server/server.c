27f1e2ddc1f6eb015126f72ba6ef227c67a817f1
Sun Kit Tsui <sunkittsui@gmail.com>
2023-03-16 10:10:51 -0500

    Cleaned up formatting and redundant headers

--- server/server.c
+++ server/server.c
@@ -1,13 +1,5 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <sys/socket.h>
 #include <sys/types.h>
-
-#include <netinet/in.h>
-#include <unistd.h>
 
 #include "server.h"
 #include "../utils/parsers.h"
