d34d56a7e57b21f6517cd1c982f2539a81dcfc76
Sun Kit Tsui <sunkittsui@gmail.com>
2023-03-22 00:23:11 -0500

    Added space between function declarations

--- utils/http.h
+++ utils/http.h
@@ -32,7 +32,8 @@ typedef struct {
 
 
 typedef struct {
-    int status;
+    int statusCode;
+    char *status;
     HttpHeaderList *headerList;
     char *body;
 } HttpResponse;
@@ -46,19 +47,28 @@ typedef struct {
     bool canDelete;
 } HttpEndPoint;
 
+
 // Constructs an empty HttpHeaderList.
 // Returns NULL if failed to construct the list
 HttpHeaderList *constructHttpHeaderList(size_t capacity);
+
+
 // Adds to the end of the HttpHeaderList.
 // Returns false if failed to add to list (failed to allocate memory for new entry)
 bool httpHeaderListAdd(HttpHeaderList *list, char *key, char *value);
+
+
 // Gets the HttpHeader in HttpHeaderList if key exists. 
 // Returns NULL if key doesn't exist
 HttpHeader *httpHeaderListGet(HttpHeaderList *list, char *key);
+
+
 // Removes HttpHeader in list with specified key
 // Returns true if removed successfully, false if key doesn't exist
 // Note: Removing HttpHeader from list frees its memory
 bool httpHeaderListRemove(HttpHeaderList *list, char *key);
+
+
 // Checks if an HttpHeader with specified key exists in list
 bool httpHeaderListContainsHeader(HttpHeaderList *list, char *key);
 
